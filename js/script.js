aebc555099bdd26a1554602a378fc9d4cc73d8ed
Luigidefilippo <defilippoluigi65@gmail.com>
2023-03-13 15:50:55 +0100

    fixed pt 2 finish

--- js/script.js
+++ js/script.js
@@ -1,28 +1,25 @@
 const containerElem = document.querySelector(".container");
 console.log(containerElem);
 
-for(let i = 1; 1<=100; i++){
+for(let i = 1;  i <=100; i++){
     if (i % 3 === 0 && i % 5 ===0){
         console.log(i,"FizzBuzz");
-        containerElem =`<div class="box">FizzBuzz</div>`;
-        boxText ="FizzBuzz";
-        boxClass="fizz-buzz";
+        // containerElem =`<div class="box">FizzBuzz</div>`;
+        // boxText ="FizzBuzz";
+        // boxClass="fizz-buzz";
 
     } else if ( i % 5 === 0){
         console.log(i, "Buzz");
-        containerElem =`<div class="box">Buzz</div>`;
-        boxText ="Buzz";
-        boxClass="buzz";
 
     }else if ( i % 3 === 0 ){
         console.log( i, "Fizz");
-        boxText ="Fizz";
-        boxClass="fizz";
+        // boxText ="Fizz";
+        // boxClass="fizz";
 
     }else{
         console.log(i);
-        boxText =i;
-        boxClass="";
+        // boxText =i;
+        // boxClass="";
     }
-    containerElem.innerHTML += `<div class="box ${boxClass}">${boxText}</div>`;
-}
+//     containerElem.innerHTML += `<div class="box ${boxClass}">${boxText}</div>`;
+    }
